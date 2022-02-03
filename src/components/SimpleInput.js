d58efc039005118947449bf1a2425211c8f13185
ran-research-lab <rafael.arce@upr.edu>
2022-02-03 13:53:03 -0400

    Finished L202 (basic validation)

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,7 +3,9 @@ import { useState, useRef } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+
+  // L202. (A) create a state for the validity of the input
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,20 +13,32 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    // L202 (B) conditional to determine validity, if not
+    // valid then change state to false
     if (enteredName.trim() === "") {
-      setEnteredNameIsValid("false");
+      console.log("setEnteredNameIsValid --> false");
+      setEnteredNameIsValid(false);
       return;
     }
 
-    setEnteredNameIsValid("true");
+    // L202 (C) change the state to true if valid
+    setEnteredNameIsValid(true);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
     setEnteredName("");
   };
 
+  // L202 (D) you may choose to change the style of the form
+  // depending on validity
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      {/* L202 (E) the variable nameInputClasses controls the style of the form*/}
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -34,6 +48,7 @@ const SimpleInput = (props) => {
           value={enteredName}
         />
       </div>
+      {/* L202 (F) and the state controls if the message shows */}
       {!enteredNameIsValid && (
         <p className="error-text">Name must not be empty.</p>
       )}
