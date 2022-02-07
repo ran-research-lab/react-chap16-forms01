84a34cf0414638c6cd11d3779b8a2596beb43c75
ran-research-lab <rafael.arce@upr.edu>
2022-02-07 16:15:17 -0400

    Assignment 5: finished (bien tecato)

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
@@ -19,12 +19,14 @@ const SimpleInput = (props) => {
   //   }
   // }, [enteredNameIsValid]);
 
+  const [enteredEmail, setEnteredEmail] = useState("");
   // L205 (B) We refactored the validation functions to this..
   const enteredNameIsValid = enteredName.trim() !== "";
+  const enteredEmailIsValid = enteredEmail.includes("@");
 
   // L206 (B) For a form that has more components we would have a more
   //          complex expression
-  let formIsValid = enteredNameIsValid;
+  let formIsValid = enteredNameIsValid && enteredEmailIsValid;
 
   // useEffect(() => {
   //   // L206 (A) set the overall form validity
@@ -44,6 +46,21 @@ const SimpleInput = (props) => {
     // }
   };
 
+  // L202. (A) create a state for the validity of the input
+  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+
+  // L203 (B) create a state to monitor if touched
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
+    console.log(enteredEmail, enteredEmailIsValid);
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
     // L203 (D) Any time that the user submits, we consider it touched
@@ -59,15 +76,17 @@ const SimpleInput = (props) => {
 
     // L205 (C) We still want to avoid submitting if invalid, but now
     //          now we use the deduced variable
-    if (!enteredNameIsValid) return;
+    if (!enteredNameIsValid || !enteredEmailIsValid) return;
 
     // L202 (C) change the state to true if valid
     // setEnteredNameIsValid(true);
 
     setEnteredName("");
+    setEnteredEmail("");
 
     // L205 (D) Once the form is submitted it should acta as untouched
     setEnteredNameTouched(false);
+    setEnteredEmailTouched(false);
   };
 
   // L204 (A) the blur handler sets the touched to true
@@ -84,10 +103,15 @@ const SimpleInput = (props) => {
 
   // L203 (C) now is invalid is combo of two conditions
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
 
   // L202 (D) you may choose to change the style of the form
   // depending on validity
   const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClasses = emailInputIsInvalid
     ? "form-control invalid"
     : "form-control";
 
@@ -105,10 +129,25 @@ const SimpleInput = (props) => {
           value={enteredName}
         />
       </div>
+
+      <div className={emailInputClasses}>
+        <label htmlFor="name">Your Email</label>
+        <input
+          type="text"
+          id="name"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+      </div>
+
       {/* L202 (F) and the state controls if the message shows */}
       {/* L203 (C) we use the new combo condition */}
       {nameInputIsInvalid && (
         <p className="error-text">Name must not be empty.</p>
+      )}
+      {emailInputIsInvalid && (
+        <p className="error-text">Email must not be empty.</p>
       )}
       <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
